c89a0653852700e17f50db4e33e5f98c9e97a643
asifanr21 <asif.cs25@bmsce.ac.in>
2025-12-16 11:36:47 +0530

    Add files via upload

---
+++ data.c
@@ -0,0 +1,15 @@
+#include  <stdio.h>
+
+int main() {
+    int a,b,c;
+
+    printf("enter the a and b values:");
+
+    scanf( "%d%d" ,&a,&b);
+    c=a+b;
+    printf("adding two values %d",c);
+
+    
+
+
+}
---
+++ stack.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include <conio.h>
+
+int a[0];
+top=0// 
+
+puch(x){
+    top =top+1;
+    A[top]=x;
+}
+
